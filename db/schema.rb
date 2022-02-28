b5cd8006ceabfe45db504a8c2370182a9d270cd9
Tenzin Jorden <tenden96@gmail.com>
2022-02-28 15:20:46 -0800

    Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 3) do
 
-  create_table "products", force: :cascade do |t|
+  create_table "students", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "grade"
+    t.datetime "birthdate"
   end
 
 end
